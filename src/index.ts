abf49fa3523f274436631bddbc659add7e2d9c34
nikolai <nikolai@nikolais-MacBook-Air.local>
2024-05-17 15:57:14 +0300

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import { resolve } from 'path';
 import OHLCVRouter from './routes/ohlcv';
+import "dotenv/config.js";
 
 if(require.main === module) {
     const app = express();
@@ -11,5 +12,10 @@ if(require.main === module) {
 
     app.use('/ohlcv/assets/', OHLCVRouter);
 
-    app.listen(5600);
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(
+            'Server is running on port:',
+            process.env.SERVER_PORT
+        );
+    });
 }
