940795faab48d44e0cd9e0182be3b8395b164ef9
nikolai <nikolai@nikolais-MacBook-Air.local>
2024-05-19 01:07:06 +0300

    compile .d.ts files to .js

--- executables/build.sh
+++ executables/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash nikolai
+
 yarn
 
 declare -a frontend_dependecies=("lightweight-charts" "fancy-canvas")
@@ -8,19 +10,22 @@ for library in "${frontend_dependecies[@]}"
 do
     src="$library${srcs[i]}"
 
-    rm -f public/chart/modules/$library 2> /dev/null || true
-    mkdir -p public/chart/modules/$library
-
-    cp -r \
-        node_modules/$src/* \
-        public/chart/modules/$library
-
     rm -f chart/modules/$library 2> /dev/null || true
     mkdir -p chart/modules/$library
 
     cp -r \
         node_modules/$src/* \
         chart/modules/$library
+
+    # .d.ts files don't get converted into .js file, but we still import
+    # from them causing error in chart.html
+
+    shopt -s globstar
+    for file in chart/modules/$library/**/*.d.ts; do
+        echo "${file%.d.ts}.ts"
+        touch "${file%.d.ts}.ts"
+        cat "$file" >> "${file%.d.ts}.ts"
+    done
 
     i=$((i+1))
 done
