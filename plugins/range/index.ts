a9017242fb7679d54bde5d0ac1a882d64847eaa4
nikolai <nikolai@nikolais-MacBook-Air.local>
2024-05-26 17:22:02 +0300

    fix ranges export

--- plugins/range/index.ts
+++ plugins/range/index.ts
@@ -3,49 +3,49 @@ import { DAY, getMidnightOpen } from "../../helpers";
 import { Candle, PDEnum } from "../../types/ohlcv";
 
 export function getDailyRanges(candles: Candle[]): CandleRange[] {
-    const rangesObject = candles
-        .map(c => [c, getMidnightOpen(c.time).toString()] as [Candle, string])
-        .reduce((ret, [c, key]) => ({
-            ...ret,
-            [key]: ret[key] ? [...ret[key], c] : [c],
-        }), {});
+	const rangesObject = candles
+		.map(c => [c, getMidnightOpen(c.time).toString()] as [Candle, string])
+		.reduce((ret, [c, key]) => ({
+			...ret,
+			[key]: ret[key] ? [...ret[key], c] : [c],
+		}), {});
 
-    return Object.keys(rangesObject)
-        .map(k => new DailyRange(new Date(k), rangesObject[k]))
-        .sort((a, b) => a.openTime - b.openTime);
+	return Object.keys(rangesObject)
+		.map(k => new DailyRange(new Date(k), rangesObject[k]))
+		.sort((a, b) => a.openTime - b.openTime);
 }
 
 export function checkDRPDForTimePoints(toCheck: { time: number; }[], allCandles: Candle[]) {
-    const pds = [] as PDEnum[];
-    const dailyRanges = getDailyRanges(allCandles);
+	const pds = [] as PDEnum[];
+	const dailyRanges = getDailyRanges(allCandles);
 
-    let i = 0;
-    let j = 0;
+	let i = 0;
+	let j = 0;
 
-    while(i < dailyRanges.length && j < toCheck.length) {
-        const dr = dailyRanges[i];
-        const { time } = toCheck[j];
+	while(i < dailyRanges.length && j < toCheck.length) {
+		const dr = dailyRanges[i];
+		const { time } = toCheck[j];
 
-        if (!dr.contains(time)) {
-            i++;
-            continue;
-        }
+		if (!dr.contains(time)) {
+			i++;
+			continue;
+		}
 
-        const pd = dr.getPremiumDiscountAtTime(time);
-        pds.push(pd);
-        j++;
-    }
+		const pd = dr.getPremiumDiscountAtTime(time);
+		pds.push(pd);
+		j++;
+	}
 
-    if (pds.length !== toCheck.length) {
-        throw new Error('Invlaid input');
-    }
+	if (pds.length !== toCheck.length) {
+		throw new Error('Invlaid input');
+	}
 
-    const mnos = dailyRanges.map(dr => dr.open);
-    return { pds, mnos };
+	const mnos = dailyRanges.map(dr => dr.open);
+	return { pds, mnos };
 }
 
 export class DailyRange extends CandleRange {
-    constructor(openTime: Date, candles: Candle[]) {
-        super(DAY, openTime, candles);
-    }
+	constructor(openTime: Date, candles: Candle[]) {
+		super(DAY, openTime, candles);
+	}
 }
